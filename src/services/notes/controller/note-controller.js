29b78bdfacdd335faed7efe529021bf93480a820
Robby Riandi <rbyrndi@gmail.com>
2026-05-13 04:13:40 +0700

    feat: implement error handling middleware and response utility, enhance note controller with error management

--- src/services/notes/controller/note-controller.js
+++ src/services/notes/controller/note-controller.js
@@ -1,97 +1,67 @@
 import { nanoid } from 'nanoid';
 import notes from '../notes.js';
+import { InvariantError, NotFoundError } from '../../../exceptions/index.js';
+import response  from '../../../utils/response.js';
 
-export const createNote = (req, res, next) => { 
+export const createNote = (req, res, next) => {
     const { title = 'untitled', tags, body } = req.body;
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
+
     const newNote = { title, tags, body, id, createdAt, updatedAt };
     notes.push(newNote);
 
-    const isSucess = notes.filter((note) => note.id === id).length > 0;
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
 
-    if (isSucess) {
-        return res.status(201).json({
-            status: 'success',
-            message: 'Catatan berhasil ditambahkan',
-            data: { noteId: id },
-        });
+    if (!isSuccess) {
+        return next(new InvariantError('Catatan gagal ditambahkan'));
     }
 
-    return  res.status(500).json({
-        status: 'fail',
-        message: 'Catatan gagal ditambahkan',
-    });
+    return response(res, 201, 'Catatan berhasil ditambahkan', { noteId: id });
 };
 
 
 export const getNotes = (req, res) => {
-    return res.json({
-        status: 'success',
-        data: { notes }
-    });
-};
+    return response(res, 200, 'Success', { notes: notes });
+}
 
-export const getNoteById = (req, res) => {
+
+export const getNoteById = (req, res, next) => {
     const { id } = req.params;
-    const note = notes.filter((n) => n.id === id);
+    const note = notes.find((n) => n.id === id);
 
-    if (note.length === 0) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Catatan tidak ditemukan',
-        });
+    if (!note) {
+        return next(new NotFoundError('Catatan tidak ditemukan'));
     }
 
-    return res.json({
-        status: 'success',
-        data: { note: note[0] }
-    });
+    return response(res, 200, 'Catatan sukses ditampilkan', { note: note });
 };
 
 
-export const editNoteById = (req, res) => {
+export const editNoteById = (req, res, next) => {
     const { id } = req.params;
     const { title, tags, body } = req.body;
     const updatedAt = new Date().toISOString();
-    const index = notes.findIndex((note) => note.id === id);
+    const index = notes.findIndex((n) => n.id === id);
 
-    if (index !== -1) {
-        notes[index] = {
-            ...notes[index],
-            title,
-            tags,
-            body,
-            updatedAt,
-        };
-        return res.json({
-            status: 'success',
-            message: 'Catatan berhasil diubah',
-        });
+    if (index === -1) {
+        return next(new NotFoundError('Catatan tidak ditemukan'));
     }
 
-    return res.status(404).json({
-        status: 'fail',
-        message: 'Catatan tidak ditemukan',
-    });
+    notes[index] = { ...notes[index], title, tags, body, updatedAt };
+    return response(res, 200, 'Catatan berhasil diperbarui', notes[index]);
 };
 
 
-export const deleteNoteById = (req, res) => {
+export const deleteNoteById = (req, res, next) => {
     const { id } = req.params;
-    const index = notes.findIndex((note) => note.id === id);
+    const index = notes.findIndex((n) => n.id === id);
 
-    if (index !== -1) {
-        notes.splice(index, 1);
-        return res.json({
-            status: 'success',
-            message: 'Catatan berhasil dihapus',
-        });
+    if (index === -1) {
+        return next(new NotFoundError('Catatan tidak ditemukan'));
     }
 
-    return res.status(404).json({
-        status: 'fail',
-        message: 'Catatan tidak ditemukan',
-    });
+    notes.splice(index, 1);
+    return response(res, 200, 'Catatan berhasil dihapus');
 };
